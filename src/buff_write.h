32dfc27203a5da3d13b840bc62ed9b8f5113c9b7
Agustin Linari <agustinlinari@gmail.com>
2016-10-17 22:50:04 -0300

    first commit

---
+++ src/buff_write.h
@@ -0,0 +1,12 @@
+/*
+ * $Id: print_dnames.h,v 1.1 2008/04/22 03:11:50 hmasci Exp $
+ */
+
+#ifndef _BUFF_WRITE_H
+#define _BUFF_WRITE_H
+
+int buff_write(unsigned int, char*, unsigned int);
+
+int buff_flush(unsigned int);
+
+#endif
---
+++ src/print_dnames.h
@@ -0,0 +1,12 @@
+/*
+ * $Id: print_dnames.h,v 1.1 2008/04/22 03:11:50 hmasci Exp $
+ */
+
+#ifndef _PRINT_DNAMES_H
+#define _PRINT_DNAMES_H
+
+#include "buff_write.h"
+
+void print_dnames(unsigned int, unsigned int);
+
+#endif
